4970e1e90c5e54db18beb8b8b0cb9d18c8746797
Hot-Tutorials <61880709+Hot-Tutorials@users.noreply.github.com>
2021-03-22 19:05:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const { Plugin } = require("powercord/entities");
 
-module.exports = class SmoothScroll extends (
-	Plugin
-) {
-	startPlugin() {
-		const smoothscroll = require("./smoothscroll.js");
-	}
-
-	pluginWillUnload() {
-		console.log("ok");
-	}
+module.exports = class SmoothScroll extends (Plugin) {
+	startPlugin() { const smoothscroll = require("./smoothscroll.js"); }
+	pluginWillUnload() { console.log("Restart Discord for it to unload"); }
 };
