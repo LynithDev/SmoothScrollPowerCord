3f1dd3aa52587a76b37e7dcddbf65d997cb13d58
Hot-Tutorials <61880709+Hot-Tutorials@users.noreply.github.com>
2021-03-22 19:02:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const { Plugin } = require("powercord/entities");
 const { findInReactTree } = require("powercord/util");
 const { getModule, React } = require("powercord/webpack");
 const { inject, uninject } = require("powercord/injector");
-const smoothscroll = require("./smoothscroll.js");
 
 module.exports = class SmoothScroll extends (
 	Plugin
 ) {
 	startPlugin() {
-		var fs = require("fs");
-		var script = fs.readFileSync("./mytext.txt", "utf-8");
-		eval(script);
+		const smoothscroll = require("./smoothscroll.js");
 	}
 
 	pluginWillUnload() {
