0134bbff72d0e6eb914d6077360e5335e95f77a4
Hot-Tutorials <61880709+Hot-Tutorials@users.noreply.github.com>
2021-03-22 19:04:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const { Plugin } = require("powercord/entities");
-const { findInReactTree } = require("powercord/util");
-const { getModule, React } = require("powercord/webpack");
-const { inject, uninject } = require("powercord/injector");
 
 module.exports = class SmoothScroll extends (
 	Plugin
